22ad0b56e95ef96ad9326fdccf2776bd057b7106
Faizan Anwar <faizana1298@gmail.com>
2024-04-11 19:57:32 +0530

    Added validation in vertical and Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,14 @@ class Category < ApplicationRecord
   has_many :courses, dependent: :destroy
   belongs_to :vertical
   accepts_nested_attributes_for :courses, allow_destroy: true
+
+  validate :name_must_be_unique_across_models
+
+  private
+
+  def name_must_be_unique_across_models
+    if Category.where.not(id: id).exists?(name: name) || Vertical.exists?(name: name)
+      errors.add(:name, "#{name} is already taken in Vertcial")
+    end
+  end
 end
--- app/models/vertical.rb
+++ app/models/vertical.rb
@@ -2,4 +2,14 @@ class Vertical < ApplicationRecord
 
   has_many :categories, dependent: :destroy
   accepts_nested_attributes_for :categories, allow_destroy: true
+
+  validate :name_must_be_unique_across_models
+
+  private
+
+  def name_must_be_unique_across_models
+    if Vertical.where.not(id: id).exists?(name: name) || Category.exists?(name: name)
+      errors.add(:name, "#{name} is already taken in Category")
+    end
+  end
 end
