f745a642303ad501abf1474c1427791b0d29dc93
Faizan Anwar <faizana1298@gmail.com>
2024-04-16 18:11:18 +0530

    fixing elastic search

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,7 +1,2 @@
 require 'elasticsearch/model'
-if Rails.env == 'production'
-  url = ENV["BONSAI_URL"]
-  transport_options = { request: { timeout: 250 } }
-  options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
-  Searchkick.client = Elasticsearch::Client.new(options)
-end
+Searchkick.client = Elasticsearch::Client.new(url: ENV['BONSAI_URL'])
