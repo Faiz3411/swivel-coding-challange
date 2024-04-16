228f136e9475d7e4e4c3c35baa9af0f7d2a93b6b
Faizan Anwar <faizana1298@gmail.com>
2024-04-16 20:23:18 +0530

    Added config for searchkick

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,2 +1,5 @@
 require 'elasticsearch/model'
-Searchkick.client = Elasticsearch::Client.new(url: ENV['BONSAI_URL'])
+  url = ENV["BONSAI_URL"]
+  transport_options = { request: { timeout: 250 } }
+  options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
+  Searchkick.client = Elasticsearch::Client.new(options)
