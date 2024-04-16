5d22129caf6c721c76e0dfe14267f11efe456071
Faizan Anwar <faizana1298@gmail.com>
2024-04-16 18:33:13 +0530

    Set BONSAI environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
   
-  config.elasticsearch_url = ENV['FOUNDELASTICSEARCH_URL']
+  config.elasticsearch_url = ENV['BONSAI_URL']
   
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
